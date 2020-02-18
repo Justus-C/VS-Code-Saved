3dcb7673f3da85aa5377caa920d5ee6e77d1e6c9
Justus-C <60617859+Justus-C@users.noreply.github.com>
2020-02-18 09:50:21 -0500

    Add files via upload

--- example.py
+++ example.py
@@ -9,3 +9,12 @@ def say_hello(name):
 def generate_message(name: str) -> str:
     return f"Welcome, {name}"
  
+"""
+Function that will add two numbers
+"""
+def add(a: int, b: int) -> int:
+    return a + b
+
+"""
+Takes two tuples and adds them together.
+"""
--- test_example.py
+++ test_example.py
@@ -17,3 +17,13 @@ from example import generate_message
 def test_generate_message():
     assert generate_message("Dave") == "Welcome, Dave"
     assert generate_message("Jim") == "Welcome, Jim"
+
+from example import add
+
+def test_add():
+    assert add(5, 4) == 9
+    assert add(1, 1) == 2
+
+def test_add_tuple():
+    assert add_tuple((1,1), (3,3)) == (4,4)
+    
